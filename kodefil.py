96032ee5b058ba5b4a29455e2066bb3cbedb9679
Frederik <frederik_oelund@outlook.com>
2023-01-27 14:52:51 +0100

    comitter

--- kodefil.py
+++ kodefil.py
@@ -1,3 +1,8 @@
 her findes  en ny kodefil
 appender til min fil
 
+tilføjer nye tilfølde linjer til min kodefil
+ændrig lidt i strukturen
+tilføjer her går det virkeligt ringe til kodefil
+halli hallo 
+dette burde kun eksistere i denne branch
